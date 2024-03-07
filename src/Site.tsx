5b58f0a4d7d163e0a1ffd96ea93d1b33916a9118
blackshibe <kacperszczepan@tutanota.com>
2024-03-07 22:50:16 +0100

    revamp the website because who cares anymore

--- src/Site.tsx
+++ src/Site.tsx
@@ -3,66 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./index.css";
 
-import snow from "./wallpaper/wallpaper_02.jpg";
-
-function ProjectCard({ children, href }: React.PropsWithChildren<{ href?: string }>) {
-	return (
-		<a href={href}>
-			<div className="border-zinc-700 bg-zinc-900 rounded border ease-in-out duration-100 hover:shadow-2xl cursor-pointer">
-				{children}
-			</div>
-		</a>
-	);
-}
-
-function ProjectTags({ children, date }: React.PropsWithChildren<{ date: string }>) {
-	return (
-		<div className="flex justify-between text-zinc-600 pt-4">
-			<div className="text-zinc-600 flex gap-1">{children}</div>
-			<div className="flex text-sm align-middle">{date}</div>
-		</div>
-	);
-}
-
-function MiscProjectCard({ children, href }: React.PropsWithChildren<{ href?: string }>) {
-	return (
-		<a href={href}>
-			<div className="border-zinc-700 bg-zinc-900 p-4 rounded border ease-in-out duration-100 hover:shadow-2xl cursor-pointer">
-				{children}
-			</div>
-		</a>
-	);
-}
-
-function ExperienceCard({ children }: React.PropsWithChildren<{}>) {
-	return <div className="border-zinc-700 bg-zinc-900 rounded border p-4">{children}</div>;
-}
-
-function ExperienceCardWithImage({ children }: React.PropsWithChildren<{}>) {
-	return <div className="border-zinc-700 bg-zinc-900 rounded border">{children}</div>;
-}
-
-function SocialIcon({ icon, text, link }: { icon: IconDefinition; text: string; link?: string }) {
-	return (
-		<div className="flex">
-			<div className="aspect-square w-7 text-base">
-				<FontAwesomeIcon className={"w-6 aspect-square "} icon={icon} />
-			</div>
-			<a href={link}>{text}</a>
-		</div>
-	);
-}
-
-function Header({ header, footer, href }: { header: string; footer: string; href?: string }) {
-	return (
-		<>
-			<a href={href} className="text-2xl font-bold font-ibm-mono ease-in-out duration-100">
-				{header}
-			</a>
-			<h2 className="text-1xl font-ibm-mono font-bold text-blue-400 pb-4">{footer}</h2>
-		</>
-	);
-}
+import snow from "./wallpaper/RBA_01.png";
 
 function Site() {
 	let banners = [snow];
@@ -71,13 +12,21 @@ function Site() {
 	return (
 		<div
 			style={{ backgroundImage: `url("${banners[banner]}")` }}
-			className="font-sans leading-6 font-poppins text-left text-lg bg-image underline-offset-4"
+			className="font-sans leading-6 font-ibm-mono text-left text-lg bg-image underline-offset-4"
 		>
-			<div className="from-zinc-900 bg-gradient-to-t flex justify-between flex-row">
-				<div className="card-width flex justify-between flex-col h-screen p-8 md:p-20 text-zinc-200"></div>
-			</div>
-			<div className="min-h-screen flex items-center p-40 bg-zinc-900 text-zinc-200">
-				<p>everything good ends eventually.</p>
+			<div className="custom-bg-opaque flex justify-center items-center h-screen">
+				<div className="flex w-100 gap-2 items-start flex-col p-8 justify-center custom-bg-opaque">
+					<p>blackshibe</p>
+					<p style={{ opacity: "50%" }}>professional programmer & hobbyist musician</p>
+					<hr />
+					<p>foxes and text editors</p>
+					<p>the world is a fuck</p>
+					<p>all good things must end</p>
+					<p style={{ opacity: "50%" }}>thanks, Jax</p>
+					<hr />
+					<a href="https://github.com/blackshibe/">https://github.com/blackshibe/</a>
+					<a href="https://soundcloud.com/shibesongs">https://soundcloud.com/shibesongs</a>
+				</div>
 			</div>
 		</div>
 	);
